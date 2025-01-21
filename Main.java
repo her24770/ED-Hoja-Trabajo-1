d3e534e28d395af76c8cf912a4c67b04186e2fd6
Oscar Rompich <oscarligo6@gmail.com>
2025-01-20 19:48:04 -0600

    Finalizacion de la clase Main para ejecutar el programa

--- Main.java
+++ Main.java
@@ -3,10 +3,69 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Menu \n1. Encender/Apagar \n2. IBlender");
-        int optionMenu = sc.nextInt();
-        
+        Blender blender = new Blender();
+
+        int option = 0;
+
+        while (option != 5) {
+            
+            if (blender.IsFull() == true) {
+                System.out.println("\n\nEstado de la licuadora: Llena");
+            } else {
+                System.out.println("\n\nEstado de la licuadora: Vacía");
+            }
+            
+            System.out.println("\n============Menu=========== \n\n1. Aumentar Velocidad \n2. Reducir Velocidad \n3. Llenar licuadora \n4. Vaciar Licuadora \n5. Salir");
+            System.out.print("\nSeleccione una opcion: ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+
+
+                    if (blender.IsFull() == true && blender.GetSpeed() < 9)
+                    {
+                    blender.SpeedUp();
+                    System.out.println("Velocidad actual: " + blender.GetSpeed());
+
+                    } else {
+                        System.out.println("La licuadora no esta llena o no se puede aumentar la velocidad");
+
+                    }
+
+                    break;
+                
+                case 2:
+
+                    if  (blender.GetSpeed() > 0)
+                    {
+                    blender.SpeedDown();
+                    System.out.println("Velocidad actual: " + blender.GetSpeed());
+                    } else {
+                        System.out.println("No se mpuede disminuir más la velocidad");
+                    }
+                    break;
+
+                case 3:
+
+                    blender.Fill();
+
+                    break;  
+                    
+                case 4:
+                    blender.Empty();
+                    break;
+                    
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }        
         sc.close();
     }
 }
