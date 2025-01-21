05e0b9e47954669321ca1e12574e35e4a4ed2bab
Gabrexcitado <gabrex08@gmail.com>
2025-01-21 17:29:11 -0600

    JavaDoc clase blender

--- Blender.java
+++ Blender.java
@@ -6,21 +6,35 @@ public class Blender implements IBlender{
     private boolean full;
     private int speed;
 
+    /**
+     * Constructor
+     */
     public Blender() {
         full = false;
         speed = 0;
     }
 
+    /**
+     * muestra si la licuadora está llena o no
+     * @return estado de capacidad
+     */
     @Override
     public boolean IsFull(){
         return full;
     }
     
+    /**
+     * muestra la velocidad actual de la licuadora
+     * @return numero del set de velocidad
+     */
     @Override
     public int GetSpeed(){
         return speed;
     }
 
+    /**
+     * aumenta la velocidad de la licuadora
+     */
     @Override
     public void SpeedUp(){
         if (speed != 9){
@@ -28,16 +42,25 @@ public class Blender implements IBlender{
         }
     }
 
+    /**
+     * determina que la licuadora está llena
+     */
     @Override
     public void Fill(){
         full = true;
     }
 
+    /**
+     * determina que la licuadora está vacía
+     */
     @Override
     public void Empty(){
         full = false;
     }
 
+    /**
+     * reduce la velocidad de la licuadora
+     */
     @Override
     public void SpeedDown(){
         if (speed >= 0){
